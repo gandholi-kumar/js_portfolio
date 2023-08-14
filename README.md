83cb05b9111e8e349dc9bbe81b7ec2c877f421b0
Gandholi Saiarunkumar <46054848+gandholi-kumar@users.noreply.github.com>
2023-08-14 14:51:09 +0530

    added files

---
+++ README.md
@@ -0,0 +1,4 @@
+### Links
+- ICONS: https://iconscout.com/unicons
+- FONTS: https://fonts.google.com/specimen/Poppins?query=POP
+- HSL COLORS: https://www.w3schools.com/colors/colors_hsl.asp
---
+++ js/main.js
@@ -0,0 +1,25 @@
+/* Menu show/hide */
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+showMenu("nav-close", "nav-menu");
+
+/* Menu hide on section click */
+const navLink = document.querySelectorAll(".nav__link");
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((nav) => nav.addEventListener("click", linkAction));
+
+/* Menu show Y hidden  */
+/* Menu show Y hidden  */
